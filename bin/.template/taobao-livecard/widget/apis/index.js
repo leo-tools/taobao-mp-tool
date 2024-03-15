a264e38575bfa0db013abbcb8c9f5726157873e1
Cheng Tang <theon.tang@lego.com>
2024-03-15 14:37:38 +0800

    fix: fix bug

--- bin/.template/taobao-livecard/widget/apis/index.js
+++ bin/.template/taobao-livecard/widget/apis/index.js
@@ -1,12 +1,12 @@
 import { GlobalData } from '../stores/global'
 
 export const envMap = {
-  dev: { api: '/dev', env: 'test', host: "api.cdp-tagging-dev.lego.cn", path: "/dev/lego-gift-recommendation/gio/forward/tracking" },
-  qa: { api: '/qa', env: 'test', host: "api.cdp-tagging-dev.lego.cn", path: "/qa/lego-gift-recommendation/gio/forward/tracking" },
-  online: { api: '', env: 'online', host: "api.cdp-tagging.lego.cn", path: "/lego-gift-recommendation/gio/forward/tracking" },
+  dev: { api: '/dev', env: 'test', host: 'a', path: '/dev/a' },
+  qa: { api: '/qa', env: 'test', host: 'b', path: '/qa/b' },
+  online: { api: '', env: 'online', host: 'c', path: 'c' },
 }
 
-export const env = envMap.online;
+export const env = envMap.online
 
 export const  makeRequest = async(data) => {
   try {
@@ -14,20 +14,20 @@ export const  makeRequest = async(data) => {
       'path': env.api + data.path,
       'method': data.method,
       'headers': {
-        "content-type": "application/json",
+        'content-type': 'application/json',
         kyid: GlobalData.kyid,
         jwt: GlobalData.jwt,
         ...data.headers
       },
-      'params': data.params || "",
+      'params': data.params || '',
       'body': data.body || {},
-      'exts': { "cloudAppId": data.cloudAppId || '47460', "timeout": 10000 }
-    });
-    console.log(result, "||||||||||||||||||||||||||", data.path, data.body);
+      'exts': { 'cloudAppId': data.cloudAppId || '47460', 'timeout': 10000 }
+    })
+    console.log(result, '||||||||||||||||||||||||||', data.path, data.body)
     return result
   } catch (err) {
-    console.error(`${data.path}请求失败`, err);
+    console.error(`${data.path}请求失败`, err)
 
-    return false;
+    return false
   }
 }
--- bin/.template/taobao-livecard/widget/component/mini-card/mini-card.js
+++ bin/.template/taobao-livecard/widget/component/mini-card/mini-card.js
@@ -1,12 +1,12 @@
-import { Cloud } from '@tbmp/mp-cloud-sdk';
-const cloud = new Cloud();
+import { Cloud } from '@tbmp/mp-cloud-sdk'
+const cloud = new Cloud()
 import { env } from '../../apis'
-import { gdpPageOnTime, gdpViewTrack, initGIOTracking, setGIOTrackingUser } from '../../gio'
+import { initGIOTracking } from '../../gio'
 
 // 云应用初始化
 cloud.init({
   env: env.env,
-});
+})
 $global.cloud = cloud
 initGIOTracking()
 
